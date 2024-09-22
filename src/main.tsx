9aa81583f72357ce1301f6cabc976e8a046ef720
endabelyu <endabelyuproject@gmail.com>
2024-09-22 10:56:48 +0700

    feat(main): add react router and initia react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import App from "@/App.tsx";
+import "@/index.css";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    id: "root",
+    path: "/",
+    element: <App />,
+    // errorElement: <ErrorPage />,
+    // loader: rootLoader,
+    children: [
+      // {
+      //   path: "/",
+      //   element: <HomeRoute />,
+      //   loader: homeLoader,
+      // },
+      // {
+      //   path: "/products",
+      //   element: <ProductsRoute />,
+      //   loader: productsLoader,
+      // },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);
