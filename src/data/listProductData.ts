8ed68174921053f1d808232b0dd3cb6aa6318afa
naswa <endabelyuproject@gmail.com>
2024-09-24 18:24:29 +0700

    feat(data): create data for list product component

---
+++ src/data/listProductData.ts
@@ -0,0 +1,61 @@
+interface Product {
+  id: number;
+  slug: string;
+  imageURL: string;
+  name: string;
+  price: number;
+}
+
+const listProducts: Product[] = [
+  {
+    id: 1,
+    slug: "one-piece-figurine",
+    imageURL:
+      "https://onepiece.store/wp-content/uploads/2024/02/Anime-One-Piece-Luffy-Resin-Office-Pen-Holders-Collectible-Monkey-D-Luffy-10cm-Desk-Pencil-Pot.jpg_640x640-1.webp",
+    name: "One Piece Figurine",
+    price: 150000,
+  },
+  {
+    id: 2,
+    slug: "naruto-figurine",
+    imageURL:
+      "https://onepiece.store/wp-content/uploads/2024/02/Anime-One-Piece-Luffy-Resin-Office-Pen-Holders-Collectible-Monkey-D-Luffy-10cm-Desk-Pencil-Pot.jpg_640x640-1.webp",
+    name: "Naruto Figurine",
+    price: 120000,
+  },
+  {
+    id: 3,
+    slug: "dragon-ball-figurine",
+    imageURL:
+      "https://onepiece.store/wp-content/uploads/2024/02/Anime-One-Piece-Luffy-Resin-Office-Pen-Holders-Collectible-Monkey-D-Luffy-10cm-Desk-Pencil-Pot.jpg_640x640-1.webp",
+    name: "Dragon Ball Figurine",
+    price: 170000,
+  },
+
+  {
+    id: 4,
+    slug: "attack-on-titan-figurine",
+    imageURL:
+      "https://onepiece.store/wp-content/uploads/2024/02/Anime-One-Piece-Luffy-Resin-Office-Pen-Holders-Collectible-Monkey-D-Luffy-10cm-Desk-Pencil-Pot.jpg_640x640-1.webp",
+    name: "Attack on Titan Figurine",
+    price: 200000,
+  },
+  {
+    id: 5,
+    slug: "attack-on-titan-figurine",
+    imageURL:
+      "https://onepiece.store/wp-content/uploads/2024/02/Anime-One-Piece-Luffy-Resin-Office-Pen-Holders-Collectible-Monkey-D-Luffy-10cm-Desk-Pencil-Pot.jpg_640x640-1.webp",
+    name: "Attack on Titan Figurine",
+    price: 200000,
+  },
+  {
+    id: 6,
+    slug: "attack-on-titan-figurine",
+    imageURL:
+      "https://onepiece.store/wp-content/uploads/2024/02/Anime-One-Piece-Luffy-Resin-Office-Pen-Holders-Collectible-Monkey-D-Luffy-10cm-Desk-Pencil-Pot.jpg_640x640-1.webp",
+    name: "Attack on Titan Figurine",
+    price: 200000,
+  },
+];
+
+export default listProducts;
