c844e4bee4b1fa3c7d8cf3f74b8a1cb48613bbf0
naswa <endabelyuproject@gmail.com>
2024-09-24 18:22:57 +0700

    feat(component): create new componen for heroCarousel for homepageto showing heroImage

---
+++ src/components/shared/heroCaroursel.tsx
@@ -0,0 +1,32 @@
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "../ui/carousel";
+import imageNakamas from "@/data/heroCarouselData";
+
+const HeroCarousel = () => {
+  return (
+    <div className=''>
+      <Carousel className='mx-auto p-4 '>
+        <CarouselContent className='h-[28rem] '>
+          {imageNakamas.map(({ id, src, alt, position }) => (
+            <CarouselItem key={id} className='rounded-md'>
+              <img
+                src={`${src}`}
+                alt={alt}
+                className={`h-full w-full object-cover object-[${position}] rounded-md `}
+              />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
+  );
+};
+
+export default HeroCarousel;
