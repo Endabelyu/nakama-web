4d363a9dc60233ed01dc78c5b0b58840106f13b8
naswa <endabelyuproject@gmail.com>
2024-09-26 17:01:31 +0700

    doc(type): create new file type for writing type inside project

---
+++ src/types/index.ts
@@ -0,0 +1,66 @@
+export type User = {
+  id: string;
+
+  fullname?: string;
+  username: string;
+  email: string;
+
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export type Product = {
+  ok: boolean;
+  message: string;
+  data: ProductData[];
+  pagination: paginationData;
+};
+export type ProductDetail = {
+  ok: boolean;
+  message: string;
+  data: ProductData;
+};
+export type paginationData = {
+  page: number;
+  totalPages: number;
+  total: number;
+};
+export type ProductData = {
+  id: string;
+  name: string;
+  slug: string;
+  price: number;
+  imageURL: string;
+  category: string;
+  description: string;
+  sku: string;
+  stock: number;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export type Cart = {
+  id: string;
+
+  userId: string | null;
+  status: string;
+
+  items: CartItem[];
+
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export type CartItem = {
+  id: string;
+
+  quantity: number;
+
+  productId: string;
+  product: Product;
+
+  cartId: string;
+
+  createdAt: Date;
+  updatedAt: Date;
+};
