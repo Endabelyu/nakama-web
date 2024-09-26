2583c425badcde2813cf91c0b72dc700d0368cbd
naswa <endabelyuproject@gmail.com>
2024-09-26 16:58:19 +0700

    feat(shared): create new component and breadcrum, and add function for generate path for breadcrumb component

---
+++ src/components/shared/breadcrumbs/breadcrumbs-logic.ts
@@ -0,0 +1,24 @@
+import { useLocation } from "react-router-dom";
+
+export const useBreadcrumbs = () => {
+  const location = useLocation();
+  const paths = location.pathname.split("/").filter(Boolean);
+
+  const items = paths.map((path, index) => {
+    const routeTo = "/" + paths.slice(0, index + 1).join("/");
+    const label =
+      index === paths.length - 1 ? formatLastSegment(path) : capitalize(path);
+    return { label, path: routeTo };
+  });
+  console.log([{ label: "Nakama's", path: "/" }, ...items], "testt");
+  // Add the "Home" item at the beginning
+  return [{ label: "Nakama's", path: "/" }, ...items];
+};
+
+// Utility function to capitalize the first letter of a string
+const capitalize = (str: string) => str.charAt(0).toUpperCase() + str.slice(1);
+
+// Format the last segment for display (e.g., "one-piece-figurine" to "One Piece Figurine")
+const formatLastSegment = (str: string) => {
+  return str.split("-").map(capitalize).join(" ");
+};
---
+++ src/components/shared/breadcrumbs/breadcrumbs.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "../../ui/breadcrumb";
+import { useBreadcrumbs } from "./breadcrumbs-logic";
+
+const Breadcrumbs = () => {
+  const breadcrumbItems = useBreadcrumbs();
+  console.log(breadcrumbItems, "item");
+  return (
+    <Breadcrumb>
+      <BreadcrumbList>
+        {breadcrumbItems.map((item, index) => (
+          <>
+            <BreadcrumbItem key={item.path}>
+              <BreadcrumbLink href={item.path} className='cursor-pointer'>
+                {item.label}
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+            {index < breadcrumbItems.length - 1 && <BreadcrumbSeparator />}
+          </>
+        ))}
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
+};
+
+export default Breadcrumbs;
