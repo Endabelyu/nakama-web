69c59fb52fd1196d6bf0484cdc47c217acbd3f3f
naswa <endabelyuproject@gmail.com>
2024-09-29 13:28:35 +0700

    feat(pagination-list): add implementation pagination, function to conditional render page number, add handlepageclick function

--- src/components/shared/pagination-list.tsx
+++ src/components/shared/pagination-list.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   Pagination,
   PaginationContent,
@@ -8,82 +8,136 @@ import {
   PaginationNext,
   PaginationPrevious,
 } from "../ui/pagination";
+import { Button } from "../ui/button";
+import {
+  DoubleArrowLeftIcon,
+  DoubleArrowRightIcon,
+} from "@radix-ui/react-icons";
+import { useLocation, useNavigate } from "react-router-dom";
 
 interface PaginationListProps {
   currentPage: number;
   totalPages: number;
   total: number;
-  // onPageChange: (page: number) => void;
+  onPageChange: (page: number) => void;
 }
+
 const PaginationList: React.FC<PaginationListProps> = ({
   currentPage,
   totalPages,
-  // onPageChange,
 }) => {
-  //   const handlePrevious = () => {
-  //     if (currentPage > 1) onPageChange(currentPage - 1);
-  //   };
-
-  //   const handleNext = () => {
-  //     if (currentPage < totalPages) onPageChange(currentPage + 1);
-  //   };
-
+  console.log(currentPage, "currentPage");
+  const location = useLocation();
+  const startPage = 1;
   const [visiblePages, setVisiblePages] = useState<number[]>([]);
+  const navigate = useNavigate();
+  useEffect(() => {
+    setVisiblePages(getPageNumbers(currentPage));
+  }, [currentPage]);
 
-  const getPageNumbers = () => {
+  const getPageNumbers = (current: number) => {
     const pageNumbers: number[] = [];
-    const maxVisiblePages = 5;
+    const maxVisiblePages = 7;
 
-    // Add first pages
-    for (let i = 1; i <= Math.min(maxVisiblePages, totalPages); i++) {
-      pageNumbers.push(i);
-    }
+    if (totalPages <= maxVisiblePages) {
+      // Show all pages if total pages are less than or equal to max visible pages
+      for (let i = 1; i <= totalPages; i++) {
+        pageNumbers.push(i);
+      }
+    } else {
+      // Always show the first page
 
-    // Add ellipsis if needed
-    if (totalPages > maxVisiblePages + 1) {
-      //   pageNumbers.push("ellipsis");
+      pageNumbers.push(startPage);
+
+      // Show pages around the current page
+      let start = Math.max(2, current - 2);
+      let end = Math.min(totalPages - 1, current + 1);
+
+      if (current < 5) {
+        start = startPage + 1;
+        end = totalPages < maxVisiblePages ? totalPages : maxVisiblePages;
+      } else if (
+        current >= startPage + 4 &&
+        current < totalPages - 3 &&
+        current < totalPages
+      ) {
+        start = current - 1;
+        end = Number(current) + 1;
+      } else if (current - 3 || current === totalPages) {
+        start = totalPages - 4;
+        end = totalPages - 1;
+      }
+
+      for (let i = start; i <= end; i++) {
+        console.log(i, "i");
+        pageNumbers.push(i);
+      }
+
+      // Add ellipsis if needed
+      if (current > startPage + 3) pageNumbers.splice(1, 0, -1); // Ellipsis after 1
+      if (current !== totalPages && current < totalPages - 3 && totalPages - 1)
+        pageNumbers.push(-2); // Ellipsis before last page
+
+      // Always show the last page
       pageNumbers.push(totalPages);
     }
-
     return pageNumbers;
   };
 
-  const handleEllipsisClick = () => {
-    const newPages = [];
-    const start = Math.min(currentPage + 1, totalPages - 4);
-    const end = Math.min(start + 4, totalPages);
-
-    for (let i = start; i <= end; i++) {
-      newPages.push(i);
+  const handlePageClick = (page: number) => {
+    console.log(page, "page");
+    if (page > 0 && page <= totalPages) {
+      console.log(
+        location,
+        location.pathname.includes("?"),
+        "location.pathname",
+      );
+      if (!location.search.includes("?")) {
+        navigate(`?page=${page}`);
+      } else {
+        if (location.search.includes("page")) {
+          const params = new URLSearchParams(location.search);
+          params.set("page", page.toString());
+          navigate(`?${params.toString()}`);
+        } else {
+          navigate(`${location.search}&page=${page}`);
+        }
+      }
     }
-
-    setVisiblePages(newPages);
   };
-  const displayedPages =
-    visiblePages.length > 0 ? visiblePages : getPageNumbers();
 
-  //   console.log(displayedPages, "diss");
   return (
-    <div>
+    <div className=''>
       <Pagination>
         <PaginationContent>
+          <PaginationItem className='-mr-4'>
+            <Button
+              variant='outline'
+              className='h-8 w-8 p-0 lg:flex bg-transparent text-listHat hover:bg-transparent hover:border-transparent shadow-none hover:scale-110'
+              onClick={() => handlePageClick(1)}
+              disabled={currentPage === 1}
+            >
+              <DoubleArrowLeftIcon className='h-4 w-4' />
+            </Button>
+          </PaginationItem>
           <PaginationItem>
-            <PaginationPrevious />
+            <PaginationPrevious
+              onClick={() => handlePageClick(currentPage - 1)}
+              aria-disabled={currentPage === 1}
+            />
           </PaginationItem>
 
-          {displayedPages.map((page, index) => (
+          {visiblePages.map((page, index) => (
             <PaginationItem key={index}>
-              {page > 5 ? (
-                <PaginationEllipsis
-                  onClick={handleEllipsisClick}
-                  className='cursor-pointer'
-                />
+              {page < 0 ? (
+                <PaginationEllipsis className='cursor-pointer' />
               ) : (
                 <PaginationLink
-                  href='#'
-                  //   onClick={() => onPageChange(page as number)}
-                  isActive={currentPage === (page as number)}
-                  className='active:bg-transparent'
+                  onClick={() => handlePageClick(page)}
+                  isActive={currentPage === page}
+                  className={`cursor-pointer ${
+                    currentPage === page ? "bg-strawHat" : ""
+                  }`}
                 >
                   {page}
                 </PaginationLink>
@@ -92,7 +146,20 @@ const PaginationList: React.FC<PaginationListProps> = ({
           ))}
 
           <PaginationItem>
-            <PaginationNext />
+            <PaginationNext
+              onClick={() => handlePageClick(currentPage + 1)}
+              aria-disabled={currentPage === totalPages}
+            />
+          </PaginationItem>
+          <PaginationItem className='-ml-4'>
+            <Button
+              variant='outline'
+              className='h-8 w-8 p-0 lg:flex bg-transparent text-listHat hover:bg-transparent hover:border-none hover:border-transparent shadow-none hover:scale-110'
+              onClick={() => handlePageClick(totalPages)}
+              disabled={currentPage === totalPages}
+            >
+              <DoubleArrowRightIcon className='h-4 w-4' />
+            </Button>
           </PaginationItem>
         </PaginationContent>
       </Pagination>
